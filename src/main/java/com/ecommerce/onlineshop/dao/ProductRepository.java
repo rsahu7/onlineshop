00683fbcc0c3668d5bb4b8d7dc661127d881aa0a
rsahu7 <sahu.roshan730@gmail.com>
2021-09-22 22:25:01 +0530

    display db data on UI

--- src/main/java/com/ecommerce/onlineshop/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/onlineshop/dao/ProductRepository.java
@@ -2,9 +2,12 @@ package com.ecommerce.onlineshop.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.onlineshop.entity.Product;
 
+
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
